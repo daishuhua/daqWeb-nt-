6e8b184b8da95af1233ae86303e31766cd11972d
daishuhua <daishuhua@139.com>
2017-02-03 18:27:23 +0800

    init commit

---
+++ src/main/java/com/guoanshequ/data/DaqWebApplication.java
@@ -0,0 +1,21 @@
+package com.guoanshequ.data;
+
+import com.guoanshequ.data.resources.ResourceMarker;
+import org.glassfish.jersey.jackson.JacksonFeature;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.server.mvc.mustache.MustacheMvcFeature;
+
+/**
+ * Created by daishuhua on 17-2-3.
+ */
+public class DaqWebApplication extends ResourceConfig {
+    /**
+     * Create a new resource configuration without any custom properties or
+     * resource and provider classes.
+     */
+    public DaqWebApplication() {
+        this.packages(ResourceMarker.class.getPackage().getName());
+        this.register(MustacheMvcFeature.class);
+        this.register(JacksonFeature.class);
+    }
+}
---
+++ src/main/java/com/guoanshequ/data/resources/ResourceMarker.java
@@ -0,0 +1,7 @@
+package com.guoanshequ.data.resources;
+
+/**
+ * Created by daishuhua on 17-2-3.
+ */
+public interface ResourceMarker {
+}
